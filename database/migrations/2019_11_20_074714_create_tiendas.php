cdb6e9a1e6c741a9298a2fdabc0811b99c6fd40b
“ajuanenama” <ajuanenama@zubirimanteo.com>
2019-11-20 08:54:51 +0100

    mejora home

---
+++ database/migrations/2019_11_20_074714_create_tiendas.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTiendas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tiendas', function (Blueprint $table) {
+            $table->bigIncrements('id', 20);
+            $table->string('name', 20);
+            $table->string('description');
+            $table->string('password', 255);
+            $table->string('email', 50);
+            $table->bigIncrements('likes', 11);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tiendas');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,11 @@
 
   Completa el ejercicio 1 y nombra AQUI al menos dos ventajas y dos desventajas que tenga el uso de un framework en el desarrollo de aplicaciones web.
 
+  VENTAJAS
+  - Permite organizarse mejor a la hora de trabajar con el proyecto
+  - Suele simplificar mucho el codigo y resulta mas sencillo trabajar con el.
 
+  DESVENTAJAS
+  - Siempre se debe tener la última versión disponible, ay que sino se pueden generar errores debido al cambio de algun comando o codigo del mismo.
+  - Es necesario saber como funciona, debido a que normalmente suele usar una sintaxis y comandos diferentes al propio lenguaje
 @endsection
